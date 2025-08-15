cee0a1435a46c72980a0129802f90d111f4b1b81
Amzhar Afarka <amzhar.afarka@gmail.com>
2025-08-15 11:56:04 +0700

    TEST

---
+++ soal1.js
@@ -0,0 +1,7 @@
+// Soal 1
+// Hari Selasa, Tanggal 5, Bulan Agustus, Tahun 2025
+// Amzhar Afarka X PPLG 2
+
+console.log("**********************************")
+console.log("UJIAN ALGORITMA Amzhar Afarka")
+console.log("-------------------------------------------------------")
---
+++ soal2.js
@@ -0,0 +1,6 @@
+// Soal 2
+// Hari Selasa, Tanggal 5, Bulan Agustus, Tahun 2025
+// Amzhar Afarka X PPLG 2
+
+console.log("aku kaya")
+console.log("123,45")
